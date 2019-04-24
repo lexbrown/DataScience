9ad008999244d42296f07288f85f7f8e810e80e1
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-04-24 13:06:59 +0300

    Create Match_points_with_polygons.py

---
+++ Match_points_with_polygons.py
@@ -0,0 +1,13 @@
+from shapely.geometry import Point, Polygon
+import geopandas
+
+polys = geopandas.GeoSeries({
+    'foo': Polygon([(5, 5), (5, 13), (13, 13), (13, 5)]),
+    'bar': Polygon([(10, 10), (10, 15), (15, 15), (15, 10)]),
+})
+
+_pnts = [Point(3, 3), Point(8, 8), Point(11, 11)]
+pnts = geopandas.GeoDataFrame(geometry=_pnts, index=['A', 'B', 'C'])
+pnts = pnts.assign(**{key: pnts.within(geom) for key, geom in polys.items()})
+
+print(pnts)
