1e07dfe5441a8b129109a4e1ff5f7fd61b09f07a
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-04-30 10:55:34 +0300

    Update k-neighbors.py

--- k-neighbors.py
+++ k-neighbors.py
@@ -1,7 +1,31 @@
-# Классификация методом k ближайши соседей
+# ML Iris
 
 import numpy as np
 import pandas as pd
-import scipy as sp
 import matplotlib.pyplot as plt
-import sklearn as sl
+import scipy as sp
+from sklearn.datasets import load_iris
+import mglearn
+
+iris_dataset = load_iris()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], 
+                                                    iris_dataset['target'], 
+                                                    random_state=0)
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+grr = pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+                        hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+
+knn.fit(X_train, y_train)
+
+X_new = np.array([[5, 2.9, 1, 0.2]])
+prediction = knn.predict(X_new)
+
+y_pred = knn.predict(X_test)
+print("Правильность на тестовом наборе: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(knn.score(X_test, y_test)))
