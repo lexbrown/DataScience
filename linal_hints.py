bf63f5f31288abe86674d0dc451d96cd8b5278f1
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-05-12 16:58:03 +0300

    Add files via upload

---
+++ linal_hints.py
@@ -0,0 +1,10 @@
+#linal hints
+import numpy as np
+import numpy.linalg as nplin
+
+matrix = np.array([[5, 3, -8, -4],
+                   [7.5, 0.5, -1, -7],
+                   [-2.5, 1.5, -4, 1],
+                   [10, -3, 8, -8]])
+
+print(round(nplin.det(matrix), 3))
