3c8bc04ae9cf2bf6148f731adfa9c94170070c6e
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-05-12 17:04:49 +0300

    Update linal_hints.py

--- linal_hints.py
+++ linal_hints.py
@@ -7,5 +7,5 @@ matrix = np.array([[5, 3, -8, -4],
                    [-2.5, 1.5, -4, 1],
                    [10, -3, 8, -8]])
 
-print(round(nplin.det(matrix), 3))
+print(round(nplin.det(matrix), 3)) #solve the inaccuracy
 #the answer should be equal to 15
