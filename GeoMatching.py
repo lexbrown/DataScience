99ec84e1d8724dacd66bb7010948066bb276c026
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-06-02 23:12:21 +0300

    Update GeoMatching.py

--- GeoMatching.py
+++ GeoMatching.py
@@ -12,7 +12,6 @@ for i in range(300):
         if location != None:
             ptz_final.loc[i, 'longitude'] = location.longitude
             ptz_final.loc[i, 'latitude'] = location.latitude
-            #123
     else:
         if ptz_final.loc[i, 'address'] == ptz_final.loc[i-1, 'address']:
             ptz_final.loc[i, 'longitude'] = ptz_final.loc[i-1, 'longitude']
